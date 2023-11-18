21a354f573de1d4f97093903c73626547294ce91
kyleleongg <148837444+kyleleongg@users.noreply.github.com>
2023-11-18 13:28:52 -0800

    Update datapath.sv

--- datapath.sv
+++ datapath.sv
@@ -1,4 +1,4 @@
-module datapath(input [15:0]datapath_in, input[2:0]writenum, input write, input[2:0]readnum, input clk, input loada, input asel, input [1:0] ALUop, input loadc, input loadb, input[1:0] shift, input bsel, input loads, input[1:0] vsel, output Z_out, output[15:0] datapath_out);
+module datapath(input [15:0]mdata, input[15:0]sximm8, input[7:0]PC, input[15:0]C, input[2:0]writenum, input write, input[2:0]readnum, input clk, input loada, input asel, input [1:0] ALUop, input loadc, input loadb, input[1:0] shift, input bsel, input loads, input[1:0] vsel, output Z_out, output[15:0] datapath_out);
 
 reg [15:0] data_in, Ain, Bin , in; 
 reg [15:0] data_out, sout, out; 
@@ -10,7 +10,8 @@ reg [15:0] datapath_OUT;
 assign Z_out = Z_OUT; 
 assign datapath_out = datapath_OUT; 
 
-
+assign PC = 8'b00000000;
+assign mdata = 16'b0000000000000000;
 
 //1
 regfile REGFILE(.data_in(data_in), .writenum(writenum), .write(write), .readnum(readnum), .clk(clk), .data_out(data_out)); 
@@ -55,10 +56,12 @@ always@(*)begin
 end 
 
 //9
-always@(*)begin 
+    always@(*)begin             //changing the input MUX for regfile
     case(vsel)
-    1'b1: data_in = datapath_in; 
-    1'b0: data_in = datapath_OUT; 
+    2'b00: data_in = C;
+    2'b01: data_in = PC;
+    2'b10: data_in = sximm8;
+    2'b11: data_in = mdata;
     endcase
 end 
 
