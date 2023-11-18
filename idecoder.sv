6e07e89ca279380945a64557598158397787ebc7
Taranehhhhhhhh <115417532+Taranehhhhhhhh@users.noreply.github.com>
2023-11-18 15:15:06 -0800

    Update idecoder.sv

--- idecoder.sv
+++ idecoder.sv
@@ -12,52 +12,73 @@ output [1:0] shift;
 output [2:0] readnum; 
 output [2:0] writenum; 
 
-reg clk 
-reg [15:0] ins_reg; 
+reg clk; 
+wire [15:0] ins_reg; 
+reg [2:0]Readnum; 
+assign readnum = Readnum; 
+
+reg[2:0]Writenum; 
+assign writenum = Writenum; 
 
 // the register to store the value of in 
-always@(posedge clk) begin 
-    if(load)begin 
-        ins_reg<= in; 
-    end 
-end 
 
-assign [15:13]ins_reg = opcode; 
-assign [12:11]ins_reg = op; 
-assign [12:11]ins_reg = ALUop; 
-assign [4:3]ins_reg = shift;
+module ins_r(input clk, input load, input [15:0]in , output [15:0]out); 
+
+always@(posedge clk) begin 
+
+    reg [15:0]OUT; 
+    assign OUT = out; 
+
+    if(load)begin 
+        OUT <= in; 
+    end 
+end
+endmodule: ins_r 
+
+
+
+
+ins_r DUT3(.load(load), .in(in), .out(ins_reg)); 
+
+assign ins_reg[15:13] = opcode; 
+assign ins_reg[12:11] = op; 
+assign ins_reg[12:11] = ALUop; 
+assign ins_reg[4:3]= shift;
 
 
 //the three select mux for readnum and writenum 
 
 always@(*)begin 
     case(nsel)
-    3'b000: [10:8]ins_reg = readnum; 
-    3'b001: [7:5]ins_reg = readnum; 
-    3'b010: [2:0]ins_reg = readnum; 
-    default: readnum = 3'bxxx; 
+        3'b000: Readnum = ins_reg[10:8]; 
+        3'b001: Readnum = ins_reg[7:5]; 
+        3'b010: Readnum = ins_reg[2:0]; 
+        default: Readnum = 3'bxxx; 
     endcase
 end 
 
 always@(*)begin 
     case(nsel)
-    3'b000: [10:8]ins_reg = writenum; 
-    3'b001: [7:5]ins_reg = writenum; 
-    3'b010: [2:0]ins_reg = writenum; 
-    default: writenum = 3'bxxx; 
+        3'b000: Writenum = ins_reg[10:8]; 
+        3'b001: Writenum = ins_reg[7:5]; 
+        3'b010: Writenum = ins_reg[2:0]; 
+        default: Writenum = 3'bxxx; 
     endcase
-end 
+end
+
+
+
 
 reg[4:0] imm5; 
-assign [4:0]ins_reg = imm5; 
+assign ins_reg[4:0] = imm5; 
 
-sign_ext5 DUT1(.imm5(in), .sximm5(out)); 
+sign_ext5 DUT1(.in(imm5), .out(sximm5)); 
 
 
 reg[7:0] imm8; 
-assign [7:0]ins_reg = imm8; 
+assign ins_reg[7:0] = imm8; 
 
-sign_ext8 DUT2(.imm8(in), .sximm8(out)); 
+sign_ext8 DUT2(.in(imm8), .out(sximm8)); 
 
 // sign extension for a 5-bit input 
 module sign_ext5([4:0] in, [15:0] out); 
@@ -67,7 +88,7 @@ assign out = 16'(signed'(in));
 endmodule: sign_ext5 
 
 
-//sign extension for an 8-bit input 
+//sign extension for a 8-but input 
 module sign_ext8([7:0] in, [15:0] out); 
 
 assign out = 16'(signed'(in));
