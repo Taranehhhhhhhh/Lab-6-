a92d3d9f787e826195484a3fb1b681043214b9ea
kyleleongg <148837444+kyleleongg@users.noreply.github.com>
2023-11-23 12:37:37 -0800

    Update datapath.sv

--- datapath.sv
+++ datapath.sv
@@ -1,4 +1,4 @@
-module datapath(input [15:0]mdata, input[15:0]sximm5, input[15:0]sximm8, input[7:0]PC, input[2:0]writenum, input write, input[2:0]readnum, input clk, input loada, input asel, input [1:0] ALUop, input loadc, input loadb, input[1:0] shift, input bsel, input loads, input[1:0] vsel, output Z_out,output N, output V, output Z, output[15:0] datapath_out);
+module datapath(input [15:0]mdata, input[15:0]sximm5, input[15:0]sximm8, input[7:0]PC, input[2:0]writenum, input write, input[2:0]readnum, input clk, input loada, input asel, input [1:0] ALUop, input loadc, input loadb, input[1:0] shift, input bsel, input loads, input[2:0] vsel, output Z_out,output N, output V, output Z, output[15:0] datapath_out);
 
 reg [15:0] data_in, Ain, Bin , in; 
 reg [15:0] data_out, sout, out; 
